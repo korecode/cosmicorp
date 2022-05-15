c0f3650a3779cb51c9b7ebcf8a060b911cfaffc0
korecode <neoshelterbox@yandex.ru>
2022-05-15 21:40:20 +0300

    making home screen

---
+++ cosmicorp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <div>
+                <span className='logo'>Cosmicorp</span>
+            </div>
+            <div className='presentation'></div>
+        </header>
+    )
+}
