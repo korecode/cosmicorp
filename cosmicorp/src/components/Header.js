04d5baefda1d016c1f16e03879794627f351e9fe
korecode <neoshelterbox@yandex.ru>
2022-05-17 04:05:07 +0300

    added cart

--- cosmicorp/src/components/Header.js
+++ cosmicorp/src/components/Header.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaShoppingBasket} from "react-icons/fa";
 
 export default function Header() {
+    let [cartOpen, setCartOpen] = useState(false)
+
     return (
         <header>
             <div>
-                <span className='logo'>Cosmicorp</span>
+                <span className='logo'>
+                    <span className='pulse'></span>
+                    Cosmicorp
+                </span>
                 <ul className='nav'>
                     <li>Про нас</li>
                     <li>Контакты</li>
                     <li>Кабинет</li>
                 </ul>
+                <FaShoppingBasket onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+                {cartOpen && (
+                    <div className='shop-cart'></div>
+                )}
             </div>
             <div className='presentation'></div>
         </header>
