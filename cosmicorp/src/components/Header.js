fae2600436b60c83cf94fecba63c7a738a0f3da4
korecode <neoshelterbox@yandex.ru>
2022-05-17 04:51:26 +0300

    adding cart

--- cosmicorp/src/components/Header.js
+++ cosmicorp/src/components/Header.js
@@ -1,7 +1,22 @@
 import React, {useState} from 'react'
 import {FaShoppingBasket} from "react-icons/fa";
+import Order from "./Order";
 
-export default function Header() {
+const showOrders = (props) => {
+    return(<div>
+        {props.orders.map(el => (
+            <Order key={el.id} item={el} />
+        ))}
+    </div>)
+}
+
+const showNothing = () => {
+    return (<div className='empty'>
+        <h2>Корзина пуста</h2>
+    </div>)
+}
+
+export default function Header(props) {
     let [cartOpen, setCartOpen] = useState(false)
 
     return (
@@ -9,7 +24,7 @@ export default function Header() {
             <div>
                 <span className='logo'>
                     <span className='pulse'></span>
-                    Cosmicorp
+                    Cosmicorp.
                 </span>
                 <ul className='nav'>
                     <li>Про нас</li>
@@ -19,7 +34,9 @@ export default function Header() {
                 <FaShoppingBasket onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
 
                 {cartOpen && (
-                    <div className='shop-cart'></div>
+                    <div className='shop-cart'>
+                        {props.orders.length > 0 ? showOrders(props) : showNothing()}
+                    </div>
                 )}
             </div>
             <div className='presentation'></div>
