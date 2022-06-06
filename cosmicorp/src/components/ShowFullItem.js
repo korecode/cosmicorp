4fa3d9930e535487dfd5dd83adbad2301f8a286d
korecode <neoshelterbox@yandex.ru>
2022-06-06 15:21:23 +0300

    cards

--- cosmicorp/src/components/ShowFullItem.js
+++ cosmicorp/src/components/ShowFullItem.js
@@ -8,8 +8,7 @@ export class ShowFullItem extends Component {
                     <img src={"./img/" + this.props.item.img} onClick={() => this.props.onShowItem(this.props.item)} alt={"Error"}/>
                     <h2>{this.props.item.title}</h2>
                     <p>{this.props.item.desc}</p>
-                    <b>{this.props.item.price}$</b>
-                    <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)}>+</div>
+
                 </div>
             </div>
         )
